wikimedia/mediawiki-extensions-Sentry

commit bad0a30696330fea11521db0bacc784f53bf7288

Add PHP error logging to Sentry extension
Bug: T85188
Change-Id: I285a5b39df0fd4205c830e3a9d2aa07129584bba

.gitignore
@@ -1,1 +1,4 @@
-node_modules
+node_modules/
+vendor/
+composer.lock
+

Sentry.php
@@ -31,6 +31,8 @@ $wgHooks['ResourceLoaderGetConfigVars'][] = 'SentryHooks::onResourceLoaderGetCon
 $wgHooks['BeforePageDisplay'][] = 'SentryHooks::onBeforePageDisplay';
 $wgHooks['UnitTestsList'][] = 'SentryHooks::onUnitTestsList';
 $wgHooks['ResourceLoaderTestModules'][] = 'SentryHooks::onResourceLoaderTestModules';
+$wgHooks['LogException'][] = 'SentryHooks::onLogException';
+SentryHooks::onRegistration();
 
 /**
  * Sentry DSN (http://raven.readthedocs.org/en/latest/config/#the-sentry-dsn)
@@ -51,4 +53,10 @@ $wgSentryWhitelist = false;
  * @var bool
  */
 $wgSentryLogOnError = true;
+
+/**
+ * Log PHP errors automatically.
+ * @var bool
+ */
+$wgSentryLogPhpErrors = true;
 

SentryHooks.php
@@ -56,4 +56,24 @@ class SentryHooks {
 		}
 		return substr( $dsn, 0, $colon_pos ) . substr( $dsn, $at_pos );
 	}
+
+	public static function onRegistration() {
+		if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+			require_once( __DIR__ . '/vendor/autoload.php' );
+		}
+	}
+
+	public static function onLogException( Exception $e ) {
+		global $wgSentryDsn, $wgSentryLogPhpErrors;
+
+		if ( !$wgSentryLogPhpErrors ) {
+			return true;
+		}
+
+		$client = new Raven_Client( $wgSentryDsn );
+		$client->captureException( $e );
+
+		return true;
+	}
 }
+

commit f5523f7606446ccea7ed7014981c50ce9cf75b7e

Re-apply "Add PHP error logging to Sentry extension"
This re-applies commit bad0a30696330fea11521db0bacc784f53bf7288,
which broke beta due to relying on a composer library not added to
mediawiki/vendor, but I1f2179bd61fff3b794a8c4fdcb5e2f45324e12bb
should have taken care of that.

(This reverts cbf7c77208dddfd5bee852565b478e77d889ecbd.)

Bug: T85188
Bug: T101252
Change-Id: I5fe94f746a95bcb9dd5b3a63e634cdd5dd5e83ec

.gitignore
@@ -1,1 +1,4 @@
-node_modules
+node_modules/
+vendor/
+composer.lock
+

Sentry.php
@@ -31,6 +31,8 @@ $wgHooks['ResourceLoaderGetConfigVars'][] = 'SentryHooks::onResourceLoaderGetCon
 $wgHooks['BeforePageDisplay'][] = 'SentryHooks::onBeforePageDisplay';
 $wgHooks['UnitTestsList'][] = 'SentryHooks::onUnitTestsList';
 $wgHooks['ResourceLoaderTestModules'][] = 'SentryHooks::onResourceLoaderTestModules';
+$wgHooks['LogException'][] = 'SentryHooks::onLogException';
+SentryHooks::onRegistration();
 
 /**
  * Sentry DSN (http://raven.readthedocs.org/en/latest/config/#the-sentry-dsn)
@@ -51,4 +53,10 @@ $wgSentryWhitelist = false;
  * @var bool
  */
 $wgSentryLogOnError = true;
+
+/**
+ * Log PHP errors automatically.
+ * @var bool
+ */
+$wgSentryLogPhpErrors = true;
 

SentryHooks.php
@@ -56,4 +56,24 @@ class SentryHooks {
 		}
 		return substr( $dsn, 0, $colon_pos ) . substr( $dsn, $at_pos );
 	}
+
+	public static function onRegistration() {
+		if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+			require_once( __DIR__ . '/vendor/autoload.php' );
+		}
+	}
+
+	public static function onLogException( Exception $e ) {
+		global $wgSentryDsn, $wgSentryLogPhpErrors;
+
+		if ( !$wgSentryLogPhpErrors ) {
+			return true;
+		}
+
+		$client = new Raven_Client( $wgSentryDsn );
+		$client->captureException( $e );
+
+		return true;
+	}
 }
+