5d5a543408892384bc79ef8d2bedc6b4b66a6f2a
Paladox <thomasmulhall410@yahoo.com>
2015-09-28 19:03:04 +0100

    Add support for running grunt in npm, Update i18n messages, Add phplint to composer.json
    Bug: T90943
    Change-Id: I192f0f6e750171d257b2a572a21b278628dbb324

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+
+	grunt.initConfig( {
+		banana: {
+			all: 'i18n/'
+		},
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**'
+			]
+		},
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'*.js',
+				'modules/**/*.js'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jsonlint', 'banana', 'jshint' ] );
+	grunt.registerTask( 'default', 'test' );
+};
