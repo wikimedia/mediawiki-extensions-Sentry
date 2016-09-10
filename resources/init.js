7de6ba2f49a7245e7e2b52d0566367d6162b3628
Gergő Tisza <gtisza@wikimedia.org>
2016-09-10 03:56:24 +0000

    Prevent Raven/TraceKit from taking over window.onerror
    TraceKit (and thus, Raven) automatically replaces window.onerror
    with its own handler (and chains the old one) when it is set up.
    The old workaround for that was to just unsubscribe the setup
    script (which loads Raven and sends errors to window.onerror
    again once it is loaded). That caused two bugs:
    * for all the errors that were triggered before Raven finished
      loading, all global.error handlers other than Raven were
      triggered twice
    * under certain timing conditions (probably two errors in very
      quick succession), mw.track could get into a state
      where there was a backlog of items in its trackQueue, in which
      case unsubscribing that handler did not help to prevent infinite
      loops: Raven's error handler called the old onerror handler,
      which pushed the error on top of trackQueue, and once Raven's
      error handler finished, it was called again with the new item.
    
    Instead, just restore the onerror handler after TraceKit's setup
    code has run. This has the disadvantage that TraceKit is not
    the top-level error handler anymore, so an exception in some
    other global.error subscriber can prevent error reporting,
    but that's an OK tradeoff for making the whole setup simpler.
    
    Bug: T143279
    Change-Id: I4b5b74a257ba98d7078c5b045abbb1e2fbf5185c

--- resources/init.js
+++ resources/init.js
@@ -3,13 +3,16 @@
 		errorCount = 0;
 
 	/**
-	 * @return {jQuery.Deferred} a deferred with the Raven.js object
+	 * @return {jQuery.Deferred} a deferred with two values: the Raven.js object and the TraceKit
+	 *   error handler
 	 */
 	function initRaven() {
 		if ( !ravenPromise ) {
 			ravenPromise = mw.loader.using( 'sentry.raven' ).then( function () {
 				var config = mw.config.get( 'wgSentry' ),
-					options = {};
+					options = {},
+					oldOnError,
+					traceKitOnError;
 
 				if ( config.whitelist ) {
 					options.whitelistUrls = config.whitelist.slice( 0 );
@@ -37,11 +40,21 @@
 					return true;
 				};
 
-				Raven.config( config.dsn, options ).install();
+				// Annoyingly, there is no way to install Raven/TraceKit without it taking over
+				// the global error handler (and chaining the old handler after itself).
+				oldOnError = window.onerror;
+				window.onerror = null;
+				try {
+					Raven.config(config.dsn, options).install();
+				} catch ( e ) {
+					window.onerror = oldOnError;
+					mw.log.error( e );
+					return $.Deferred().reject( e );
+				}
+				traceKitOnError = window.onerror;
+				window.onerror = oldOnError;
 
-				mw.trackUnsubscribe( handleGlobalError );
-
-				return Raven;
+				return $.Deferred().resolve( Raven, traceKitOnError );
 			} );
 		}
 		return ravenPromise;
@@ -57,7 +70,7 @@
 	 * @param {Object} [data.context] Additional key-value pairs to be recorded as Sentry tags
 	 */
 	function report( topic, data ) {
-		mw.sentry.initRaven().done( function ( raven ) {
+		mw.sentry.initRaven().done( function ( raven/*, traceKitOnError*/ ) {
 			var tags = { source: data.source };
 
 			if ( data.module ) {
@@ -79,10 +92,8 @@
 	 * @param {Object} data
 	 */
 	function handleGlobalError( topic, data ) {
-		mw.sentry.initRaven().done( function ( raven ) {
-			// By this point, Raven replaced the old window.onerror; we need to process errors
-			// caught before that which are queued in mw.track.
-			window.onerror.call( window, data.errorMessage, data.url, data.lineNumber, data.columnNumber,
+		mw.sentry.initRaven().done( function ( raven, traceKitOnError ) {
+			traceKitOnError.call( window, data.errorMessage, data.url, data.lineNumber, data.columnNumber,
 				data.errorObject );
 		} );
 	}
@@ -95,7 +106,7 @@
 	mw.trackSubscribe( 'global.error',  handleGlobalError );
 
 	mw.trackSubscribe( 'eventlogging.error', function ( topic, error ) {
-		mw.sentry.initRaven().done( function ( raven ) {
+		mw.sentry.initRaven().done( function ( raven/*, traceKitOnError*/ ) {
 			raven.captureMessage( error, { source: 'EventLogging' } );
 		} );
 	} );
