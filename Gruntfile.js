ba690b8e6796c30e16fdb2d8a2054cb53a29bb1d
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2019-08-23 04:39:21 +0000

    build: Updating eslint-utils to 1.4.2
    * https://npmjs.com/advisories/1118
    
    Additional changes:
    * Enable eslint caching.
    * Enable eslint's reportUnusedDisableDirectives.
    
    Change-Id: I59c73122ce5d6cb6d4f6a9f9e211944c60f4b43c

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,6 +16,10 @@ module.exports = function ( grunt ) {
 			]
 		},
 		eslint: {
+			options: {
+				cache: true,
+				reportUnusedDisableDirectives: true
+			},
 			all: [
 				'**/*.js',
 				'!resources/raven/**',
